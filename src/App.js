e44279e4bec80007f2d1d8c97513f16ac2d92404
Kenny Harris <kennyharris@Kennys-MacBook-Pro.local>
2021-11-14 20:39:14 -0600

    First upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Food Journal</h1>
+      <p>Hello, this is the home page</p>
+      <div>
+        <form action="">
+          <label htmlFor="meal">What did you eat? 
+            <textarea ></textarea>
+          </label>
+        </form>
+
+        <label htmlFor="">
+            Diet ?  
+            <select >
+              <option value="Standard">Standard</option>
+              <option value="Vegan">Vegan</option>
+              <option value="Paleo">Paleo</option>
+              <option value="Mediteranean">Mediteranean</option>
+              <option value="Pescetarian">Pescetarian</option>
+              <option value="Keto">Keto</option>
+              <option value="Vegetarian">Vegetarian</option>
+            </select>    
+        </label>
+
+          <label htmlFor="">
+            Dairy ? 
+            <input type="radio" value="No" name="option"/> No
+            <input type="radio" value="Yes" name="option"/> Yes
+          </label>
+
+          <label htmlFor="">
+            Spicy ?
+            <input type="radio" value="No" name="option"/> No
+            <input type="radio" value="Yes" name="option"/> Yes
+          </label>
+      </div>
+    </>
+    
+    
   );
 }
 

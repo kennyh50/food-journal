210f6f750323ab135b83f53d84c507b8cdebd65a
Kenny Harris <kennyharris@Kennys-MacBook-Pro.local>
2021-11-16 21:39:25 -0600

    centered everything

--- src/App.js
+++ src/App.js
@@ -2,42 +2,44 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>Food Journal</h1>
-      <p>Hello, this is the home page</p>
-      <div>
-        <form action="">
-          <label htmlFor="meal">What did you eat? 
-            <textarea ></textarea>
-          </label>
-        </form>
-
-        <label htmlFor="">
-            Diet ?  
-            <select >
-              <option value="Standard">Standard</option>
-              <option value="Vegan">Vegan</option>
-              <option value="Paleo">Paleo</option>
-              <option value="Mediteranean">Mediteranean</option>
-              <option value="Pescetarian">Pescetarian</option>
-              <option value="Keto">Keto</option>
-              <option value="Vegetarian">Vegetarian</option>
-            </select>    
-        </label>
+    <div className = 'whole-thang'>
+      <div className="center-content">
+        <h1>Food Journal</h1>
+        <div>
+          <form action="">
+            <label htmlFor="meal">What did you eat? 
+              <input type="text" />
+            </label>
+          </form>
 
           <label htmlFor="">
-            Dairy ? 
-            <input type="radio" value="No" name="option"/> No
-            <input type="radio" value="Yes" name="option"/> Yes
+              Diet ?  
+              <select >
+                <option value="Standard">Standard</option>
+                <option value="Vegan">Vegan</option>
+                <option value="Paleo">Paleo</option>
+                <option value="Mediteranean">Mediteranean</option>
+                <option value="Pescetarian">Pescetarian</option>
+                <option value="Keto">Keto</option>
+                <option value="Vegetarian">Vegetarian</option>
+              </select>    
           </label>
 
-          <label htmlFor="">
-            Spicy ?
-            <input type="radio" value="No" name="option"/> No
-            <input type="radio" value="Yes" name="option"/> Yes
-          </label>
-      </div>
-    </>
+            <label htmlFor="">
+              Dairy ? 
+              <input type="radio" value="No" name="option"/> No
+              <input type="radio" value="Yes" name="option"/> Yes
+            </label>
+
+            <label htmlFor="">
+              Spicy ?
+              <input type="radio" value="No" name="option"/> No
+              <input type="radio" value="Yes" name="option"/> Yes
+            </label>
+        </div>
+        </div>
+      
+    </div>
     
     
   );
